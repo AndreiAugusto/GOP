ab0968c89a931d21e17c2c7bb377cc8178498739
ficdev <andrei5521@hotmail.com>
2023-09-06 20:26:59 -0400

    first commit

---
+++ api/src/database/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dialect: 'postgres',
+    database: 'GOPdatabase',
+    username: 'postgres',
+    password: '123',
+    host: 'localhost',
+    port: 5432,
+    logging: false
+};
